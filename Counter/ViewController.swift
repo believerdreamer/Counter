907e2c6cc82780fa52c8af20d0db90311c7d7433
Arkhip Semenov <liteplay03@ya.ru>
2023-05-15 19:21:52 +0300

    Сделано задание по спринту 3 для проверки ревьювером.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainCountLabel: UILabel!
+    var count: Int = 0
+    @IBOutlet weak var mainButton: UIButton!
+    @IBAction func pressButton(_ sender: Any) {
+        count += 1
+        mainCountLabel.text = String(count)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
     }
-
-
+    
+    
 }
 
